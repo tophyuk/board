f99de025210a916af88feaf3cb0a6129f3dfbfdd
tophyuk <sanghyuk1992@naver.com>
2023-01-10 16:27:25 +0900

    add : 게시판 목록, 게시판 추가 REST API 개발

---
+++ src/main/java/com/tophyuk/board/controller/BoardController.java
@@ -0,0 +1,23 @@
+package com.tophyuk.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @GetMapping("/post")
+    public String write() {
+        return "board/write";
+    }
+
+    @GetMapping("/list")
+    public String list() {
+        return "board/list";
+    }
+
+
+}
