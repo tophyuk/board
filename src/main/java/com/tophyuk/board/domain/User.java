8c1e26294e8c9520371aa2e1d75426a6f44f5841
tophyuk <sanghyuk1992@naver.com>
2023-01-27 18:09:24 +0900

    add : 사용자 엔티티 생성

---
+++ src/main/java/com/tophyuk/board/domain/User.java
@@ -0,0 +1,13 @@
+package com.tophyuk.board.domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User {
+
+
+}
