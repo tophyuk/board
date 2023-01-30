2aa004226227a37bbcb8f74bb96e37dc67d2143a
tophyuk <sanghyuk1992@naver.com>
2023-01-30 11:07:48 +0900

    feat : 회원가입 화면 및 사용자 엔티티 추가

--- src/main/java/com/tophyuk/board/domain/User.java
+++ src/main/java/com/tophyuk/board/domain/User.java
@@ -1,13 +1,37 @@
 package com.tophyuk.board.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 @Getter
-public class User {
+public class User extends Time{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
+    @Column(length = 8, nullable = false)
+    private String userName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String region;
+
+    @Builder
+    public User(long id, String userName, String email, String password, String region) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.region = region;
+    }
 }
