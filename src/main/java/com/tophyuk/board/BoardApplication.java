eb3e51d711a747898a9238148edb7583f3e5be34
tophyuk <sanghyuk1992@naver.com>
2023-02-21 17:37:52 +0900

    update : SecurityConfig에서 비밀번호 암호화 Bean 이동

--- src/main/java/com/tophyuk/board/BoardApplication.java
+++ src/main/java/com/tophyuk/board/BoardApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing
@@ -17,6 +18,11 @@ public class BoardApplication {
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
 		return new HiddenHttpMethodFilter();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
